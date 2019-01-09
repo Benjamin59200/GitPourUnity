f832f3cab3bb4d64ea5c84a21f060f274c58f2dd
Benjamin <b.briez@whitedev.fr>
2019-01-09 11:17:56 +0100

    Project Importation for git

---
+++ TestForGit/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+
+    public Material blueMaterial;
+    public Material redMaterial;
+    public MeshRenderer mR;
+
+	// Use this for initialization
+	void Start () {
+        mR = gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ChangeMaterial(redMaterial);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ChangeMaterial(blueMaterial);
+        }
+
+    }
+
+    void ChangeMaterial(Material materialTarget)
+    {
+        mR.material = materialTarget;
+    }
+}
---
+++ TestForGit/Assets/Scripts/ChangeScale.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour {
+
+    private Vector3 startScale;
+
+	// Use this for initialization
+	void Start () {
+        startScale = transform.localScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("the current cube is already fat now");
+            transform.localScale = new Vector3(2,2,2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("the current cube is already small now");
+            transform.localScale = startScale;
+        }
+
+    }
+}
